3964aa4c119eec3e802b3abb956f6ad08caa4c7c
imashi2001 <ranaweeraimashi880@gmail.com>
2025-04-27 07:26:31 +0530

    update routs path in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Components/UserManagement/Home.jsx';
 import AuthPage from './Components/UserManagement/AuthPage.jsx';
 import DatabaseCheck from './Components/Interactivity&Engagement/DatabaseCheck.jsx';
 import ProfileUpdate from './Components/UserManagement/ProfileUpdate.jsx';
+import TaskCorner from './Components/SkillSharing/TaskCorner.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,7 +18,7 @@ function App() {
       <Route path="/profile" element={<ProfileUpdate />} />
 
       {/*skillsharing routes*/}
-
+      <Route path="/Task-Corner" element={<TaskCorner />} />
 
 
       {/*Interactivity routes*/}
